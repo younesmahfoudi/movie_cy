bbaa9cc3958ca223c796602838d8dbe28749626f
younesmahfoudi <younesmfd@gmail.com>
2022-03-14 14:20:04 +0100

    bdd locale initialisé avec startup.py avec 5 tables grace au data set IMDB

--- bdd-local-27017/startup.py
+++ bdd-local-27017/startup.py
@@ -5,27 +5,53 @@ import os
 import pymongo
 import json
 
-MOVIE_CY_HOME = '/home/eisti/movie_cy'
+
+MOVIE_CY_TMP = './tmp'
+DATABASE = 'MOVIE_DB'
+PORT = '27017'
+TSV_LINK = 'https://datasets.imdbws.com'
+
+# COLLECTION_NAME = 'MOVIE'
+# AKAS_TSV_GZ = 'https://datasets.imdbws.com/title.akas.tsv.gz'
+# AKAS_TSV = 'title.akas.tsv.gz'
+# MOVIE_CY_HOME = '/home/eisti/movie_cy'
+
+def importCSV(database, port, collection, fileName):
+  filePath = f"{MOVIE_CY_TMP}/{fileName}.tsv"
+  fileLink = f"{TSV_LINK}/{fileName}.tsv.gz"
+  dl = f"wget -P {MOVIE_CY_TMP} {fileLink}"
+  os.system(dl)
+  gzFile = f"{fileName}.tsv.gz"
+  extract = f"gzip -d {MOVIE_CY_TMP}/{gzFile}"
+  os.system(extract)
+  com = f"mongoimport --db {database} --collection {collection} --type tsv --file {filePath} --headerline --port={port}"
+  os.system(com)
+
+def cleanTMP():
+  delete = f"rm -R {MOVIE_CY_TMP}"
+  os.system(delete)
+
+def initDB():
+  importCSV(DATABASE,PORT,'PERSONS','name.basics')
+  importCSV(DATABASE,PORT,'MOVIES','title.basics')
+  importCSV(DATABASE,PORT,'CREWS','title.crew')
+  importCSV(DATABASE,PORT,'PRINCIPALS','title.principals')
+  importCSV(DATABASE,PORT,'RATINGS','title.ratings')
+  cleanTMP()
 
 
-def import_content(filepath):
-    mng_client = pymongo.MongoClient('localhost', 27017)
-    mng_db = mng_client['MOVIE_DB'] 
-    collection_name = 'MOVIE' 
-    db_cm = mng_db[collection_name]
-    cdir = os.path.dirname(__file__)
-    file_res = os.path.join(cdir, filepath)
-    data = pd.read_csv(file_res, encoding= 'unicode_escape')
-    data_json = json.loads(data.to_json(orient='records'))
-    db_cm.remove()
-    db_cm.insert(data_json)
 
 if __name__ == "__main__":
-  filepath = 'Sample-Spreadsheet-10-rows.csv' 
-  database = 'MOVIE_DB'
-  collection_name = 'MOVIE'
-  name_file = os.path.join(MOVIE_CY_HOME, 'local', 'data.tsv')
-  test = f"string"
-  com = f"mongoimport --db {database} --collection {collection_name} --type tsv --file {name_file} --headerline --port=27017"
-  os.system(com)
+  initDB()
+  
+  # name_file = os.path.join(MOVIE_CY_TMP, 'title.akas.tsv')
+  # dl = f"wget -P {MOVIE_CY_TMP} {AKAS_TSV_GZ}"
+  # extract = f"gzip -d {MOVIE_CY_TMP}/{AKAS_TSV}"
+  # com = f"mongoimport --db {DATABASE} --collection {COLLECTION_NAME} --type tsv --file {name_file} --headerline --port={PORT}"
+  # delete = f"rm -R {MOVIE_CY_TMP}"
+  
+  # os.system(dl)
+  # os.system(extract)
+  # os.system(com)
+  # os.system(delete)
   # import_content(filepath)
