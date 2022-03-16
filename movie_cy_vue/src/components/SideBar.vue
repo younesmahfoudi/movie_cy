c72b389850aa231072d9c9f8081c7010190e2303
Etienne Tournier <tournierri@eisti.eu>
2022-03-16 22:18:26 +0100

    icon bar du menu

--- movie_cy_vue/src/components/SideBar.vue
+++ movie_cy_vue/src/components/SideBar.vue
@@ -1,37 +1,50 @@
 <template>
-  <el-row class="tac">
-    <el-col :span="12">
-      <h5 class="mb-2">Custom colors</h5>
-      <el-menu
-        active-text-color="#5a6075"
-        background-color="#545c64"
-        class="el-menu-vertical-demo"
-        default-active="2"
-        text-color="#fff"
-        @open="handleOpen"
-        @close="handleClose"
-      >
-        <el-menu-item index="1">
-          <el-icon><icon-menu /></el-icon>
-          <span>Groupe 1</span>
-        </el-menu-item>
-      </el-menu>
-    </el-col>
-  </el-row>
+  <el-col :span="3">
+    <el-menu
+      active-text-color="#faa427"
+      background-color="#5a6075"
+      class="el-menu-vertical-demo"
+      default-active="2"
+      text-color="#ffffff"
+      :style="{ height: '100%' }"
+    >
+      <el-menu-item index="1">
+        <el-avatar
+          :style="{ backgroundColor: '#faa427' }"
+          :size="50"
+          src="./src/components/icon/utilIcon/logo.png"
+        />
+      </el-menu-item>
+      <el-menu-item index="1">
+        <el-avatar
+          :style="{ backgroundColor: '#faa427' }"
+          :size="50"
+          src="./src/components/icon/ThemeIcon/animation.png"
+        />
+      </el-menu-item>
+      <el-menu-item index="2">
+        <el-avatar
+          :style="{ backgroundColor: '#faa427' }"
+          :size="50"
+          src="./src/components/icon/ThemeIcon/action.png"
+        />
+      </el-menu-item>
+      <el-menu-item index="3">
+        <el-avatar
+          :style="{ backgroundColor: '#faa427' }"
+          :size="50"
+          src="./src/components/icon/ThemeIcon/thriller.png"
+        />
+      </el-menu-item>
+      <el-menu-item index="3">
+        <el-avatar
+          :style="{ backgroundColor: '#faa427' }"
+          :size="50"
+          src="./src/components/icon/utilIcon/plus.png"
+        />
+      </el-menu-item>
+    </el-menu>
+  </el-col>
 </template>
 
-<script lang="ts" setup>
-import {
-  Location,
-  Document,
-  Menu as IconMenu,
-  Setting,
-} from '@element-plus/icons-vue'
-const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
-const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
-</script>
-
+<style lang="css" scoped></style>
