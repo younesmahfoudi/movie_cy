e6d1b90f7b41d2918c5eac582045c07674153766
Etienne Tournier <tournierri@eisti.eu>
2022-03-16 10:36:56 +0100

    ajout icon theme / perso

--- movie_cy_vue/src/components/SideBar.vue
+++ movie_cy_vue/src/components/SideBar.vue
@@ -3,7 +3,7 @@
     <el-col :span="12">
       <h5 class="mb-2">Custom colors</h5>
       <el-menu
-        active-text-color="#ffd04b"
+        active-text-color="#5a6075"
         background-color="#545c64"
         class="el-menu-vertical-demo"
         default-active="2"
@@ -11,34 +11,9 @@
         @open="handleOpen"
         @close="handleClose"
       >
-        <el-sub-menu index="1">
-          <template #title>
-            <el-icon><location /></el-icon>
-            <span>Navigator One</span>
-          </template>
-          <el-menu-item-group title="Group One">
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item one</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="1-4">
-            <template #title>item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-          </el-sub-menu>
-        </el-sub-menu>
-        <el-menu-item index="2">
+        <el-menu-item index="1">
           <el-icon><icon-menu /></el-icon>
-          <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <el-icon><document /></el-icon>
-          <span>Navigator Three</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon><setting /></el-icon>
-          <span>Navigator Four</span>
+          <span>Groupe 1</span>
         </el-menu-item>
       </el-menu>
     </el-col>
@@ -59,3 +34,4 @@ const handleClose = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
 </script>
+
