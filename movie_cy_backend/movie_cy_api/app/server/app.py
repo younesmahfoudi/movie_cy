70e47c51507faad1561c6f3ccd98a45eb055b2ef
TomSoulage <soulagetom@eisti.eu>
2022-03-17 17:29:46 +0100

    Add Group / Fast API & monggodb ok

--- movie_cy_backend/movie_cy_api/app/server/app.py
+++ movie_cy_backend/movie_cy_api/app/server/app.py
@@ -17,8 +17,3 @@ async def read_root():
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
