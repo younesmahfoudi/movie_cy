1f19917663d605fd09e983b194d601a6549ee7d9
TomSoulage <soulagetom@eisti.eu>
2022-03-17 12:13:41 +0100

    ajout vue profil

---
+++ movie_cy_vue/src/components/ProfilManager.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="profil">   
+    <div class="common-layout">
+        <el-container>
+        <el-container>
+            <el-header>  
+            <div id="header-monprofil">
+                <div> USERNAME  </div>  
+                <div> <el-avatar :size="50" :src="circleUrl" />
+                </div>
+            </div>
+            </el-header>
+            <el-main>
+                <div>
+                    <ul>
+                        <li> Mon type préféré: </li>
+                        <li> Mon second type préféré: </li>
+                        <li> Mon acteur preferé: </li>
+                        <li> Mon réalisateur preferé: </li>
+                    </ul>       
+                </div>
+                <el-button type="warning">Modifier</el-button>
+            </el-main>
+        </el-container>
+        </el-container>
+    </div>
+  </div> 
+</template>
+
+<style>
+#header-monprofil{
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+#profil{
+    display: block;
+    width: 400px;
+}
+
+#profil ul {
+        list-style: none;
+}
+
+</style>
---
+++ movie_cy_vue/src/views/Profil.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ProfilManager from "../components/ProfilManager.vue";
+</script>
+
+<template>
+  <ProfilManager />
+</template>
+
+<style>
+</style>
