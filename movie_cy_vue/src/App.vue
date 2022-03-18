af30466c416eabb0c1e70c89083f95abc4e7a3ff
younesmahfoudi <younesmfd@gmail.com>
2022-03-18 14:23:39 +0100

    vue avec bootstrap-vue-3

--- movie_cy_vue/src/App.vue
+++ movie_cy_vue/src/App.vue
@@ -3,8 +3,9 @@ import SideBar from "./components/SideBar.vue";
 </script>
 
 <template>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-warning">Warning</button>
   <SideBar />
 </template>
 
-<style>
-</style>
+<style></style>
