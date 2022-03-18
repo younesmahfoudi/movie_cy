6bb67d0807610442993c126a35ffe4cc0a499696
Adda <addamaxime@cy-tech.fr>
2022-03-18 11:56:01 +0100

    avant merge vue-etienne

--- movie_cy_vue/src/App.vue
+++ movie_cy_vue/src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import SideBar from "./components/SideBar.vue";
+import CreationGroupForm from "./components/CreationGroupForm.vue";
+
+
 </script>
 
 <template>
+<div>
   <SideBar />
+  <CreationGroupForm />
+</div>
 </template>
 
 <style>
