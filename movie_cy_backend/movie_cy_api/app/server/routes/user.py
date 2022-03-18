e07259f10abe1d07a18cc0cc58ea972af90ff2f9
TomSoulage <soulagetom@eisti.eu>
2022-03-18 11:53:56 +0100

    getUser and getUsers ok

--- movie_cy_backend/movie_cy_api/app/server/routes/user.py
+++ movie_cy_backend/movie_cy_api/app/server/routes/user.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
 
 from app.server.database import (
-    add_user
+    add_user, retrieve_user, retrieve_users
 )
 from app.server.models.user import (
     ErrorResponseModel,
@@ -13,8 +13,23 @@ from app.server.models.user import (
 
 router = APIRouter()
 
-@router.post("/", response_description="user data added into the database")
+@router.post("/", response_description="User data added into the database")
 async def add_user_data(user: UserSchema = Body(...)):
     user = jsonable_encoder(user)
     new_user = await add_user(user)
     return ResponseModel(new_user, "user added successfully.")
+
+@router.get("/", response_description="Users retrieved")
+async def get_users():
+    users = await retrieve_users()
+    if users:
+        return ResponseModel(users, "Users data retrieved successfully")
+    return ResponseModel(users, "Empty list returned")
+
+
+@router.get("/{id}", response_description="User data retrieved")
+async def get_user_data(id):
+    user = await retrieve_user(id)
+    if user:
+        return ResponseModel(user, "User data retrieved successfully")
+    return ErrorResponseModel("An error occurred.", 404, "User doesn't exist.")
