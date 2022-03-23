2094cb2a49b122b492724d356dd3a4a130b52e5b
TomSoulage <soulagetom@eisti.eu>
2022-03-23 18:29:28 +0100

    add CORS to fastapi

--- movie_cy_backend/movie_cy_api/app/server/app.py
+++ movie_cy_backend/movie_cy_api/app/server/app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from typing import Optional
 from app.server.routes.user import router as UserRouter
 
@@ -7,6 +9,25 @@ from app.server.routes.group import router as GroupRouter
 from app.server.routes.movie import router as MovieRouteur
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 
 app.include_router(GroupRouter, tags=["groups"], prefix="/groups")
 app.include_router(UserRouter, tags=["users"], prefix="/users")
