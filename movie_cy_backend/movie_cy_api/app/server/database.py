3a2e58cb1cb4e28d943b7acbdd31e00f3d742146
younesmahfoudi <younesmfd@gmail.com>
2022-03-27 20:39:13 +0200

    Back end dans un docker ok

--- movie_cy_backend/movie_cy_api/app/server/database.py
+++ movie_cy_backend/movie_cy_api/app/server/database.py
@@ -12,8 +12,8 @@ from app.server.models.group import (
 
 
 # MONGO_DETAILS = "mongodb://localhost:27017"
-# MONGO_DETAILS = "mongodb://root:pass12345@mongodb:27017"
-MONGO_DETAILS = "mongodb://root:pass12345@localhost:27017"
+MONGO_DETAILS = "mongodb://root:pass12345@mongodb:27017"
+# MONGO_DETAILS = "mongodb://root:pass12345@localhost:27017"
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
