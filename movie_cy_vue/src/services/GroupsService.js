059d0f404b36016da87677a93b3b0d04987158e9
Etienne Tournier <tournierri@eisti.eu>
2022-03-29 15:06:34 +0200

    implement seen and delete button

--- movie_cy_vue/src/services/GroupsService.js
+++ movie_cy_vue/src/services/GroupsService.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 export default {
-    createGroup(group) {
-        console.log(group);
-        axios
-            .post("http://localhost:8000/groups/", group)
-            .then((group) => group);
-    },
+  createGroup(group) {
+    console.log(group);
+    axios.post("http://localhost:8000/groups/", group).then((group) => group);
+  },
 };
