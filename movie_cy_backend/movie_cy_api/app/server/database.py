1767c5fa98ab68868c16be66d6229317901e1b64
TomSoulage <soulagetom@eisti.eu>
2022-03-31 14:46:34 +0200

    modif requête Back pour le filtrage de film

--- movie_cy_backend/movie_cy_api/app/server/database.py
+++ movie_cy_backend/movie_cy_api/app/server/database.py
@@ -295,7 +295,7 @@ def build_movies_request_filtered(
     if genreList is not None: requestParameters.append(build_genres_request(genreList))
     if starList is not None: requestParameters.append(build_stars_request(starList))
     if imdbRating is not None: requestParameters.append(build_imdbRating_request(imdbRating))
-    if requestParameters: request: dict = {"$or" : requestParameters}
+    if requestParameters: request: dict = {"$and" : requestParameters}
     return request
 
 async def retrieve_movies_filtered(
