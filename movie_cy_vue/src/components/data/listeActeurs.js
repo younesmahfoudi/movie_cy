ef037badcf75baec0e02eaa01d8103c5f7f79276
Adda <addamaxime@cy-tech.fr>
2022-03-31 19:14:21 +0200

    update on list acteurs and realisateurs

--- movie_cy_vue/src/components/data/listeActeurs.js
+++ movie_cy_vue/src/components/data/listeActeurs.js
@@ -1,100 +1,308 @@
 export const listeActeurs = [
-    {
-        value: 'Colin Farrell',
-        label: 'Colin Farrell'
-    },
-    {
-        value: 'Jeffrey Wright',
-        label: 'Jeffrey Wright'
-    },
-    {
-        value: 'Robert Pattinson',
-        label: 'Robert Pattinson'
-    },
-    {
-        value: 'Ryan Reynolds',
-        label: 'Ryan Reynolds'
-    },
-    {
-        value: ' Walker Scobell',
-        label: ' Walker Scobell'
-    },
-    {
-        value: 'Mark Ruffalo',
-        label: 'Mark Ruffalo'
-    },
-    {
-        value: 'Rosalie Chiang',
-        label: 'Rosalie Chiang'
-    },
-    {
-        value: 'Sandra Oh',
-        label: 'Sandra Oh'
-    },
-    {
-        value: 'Robert De Niro',
-        label: 'Robert De Niro'
-    },
-    {
-        value: 'Matthew McConaughey',
-        label: 'Matthew McConaughey'
-    },
-    {
-        value: 'Anne Hathaway"',
-        label: 'Anne Hathaway"'
-    },
-
-    {
-        value: 'John Travolta',
-        label: 'John Travolta'
-    },
-    {
-        value: 'Bruce Willis',
-        label: 'Bruce Willis'
-    },
-    {
-        value: 'Leonardo DiCaprio',
-        label: 'Leonardo DiCaprio'
-    },
-    {
-        value: 'Ansel Elgort',
-        label: 'Ansel Elgort'
-    },
-    {
-        value: 'Rachel Zegler',
-        label: 'Rachel Zegler'
-    },
-    {
-        value: 'Daisy Edgar-Jones',
-        label: 'Daisy Edgar-Jones'
-    },
-    {
-        value: 'Sebastian Stan',
-        label: 'Sebastian Stan'
-    },
-    {
-        value: 'News',
-        label: 'News'
-    },
-
-    {
-        value: 'Reality-TV',
-        label: 'Reality-TV'
-    },
-    {
-        value: 'Romance',
-        label: 'Romance'
-    },
-    {
-        value: 'Jojo T. Gibbs',
-        label: 'Jojo T. Gibbs'
-    },
-    {
-        value: 'Adam Nee',
-        label: 'Adam Nee'
-    },
-    {
-        value: 'Sandra Bullock',
-        label: 'Sandra Bullock'
-    },
-]
+    [
+      {
+        value: "Al Pacino",
+        label: "Al Pacino",
+      },
+      {
+        value: "Alain Delon",
+        label: "Alain Delon",
+      },
+      {
+        value: "Alfre Woodard",
+        label: "Alfre Woodard",
+      },
+      {
+        value: "Benicio Del Toro",
+        label: "Benicio Del Toro",
+      },
+      {
+        value: "Brad Pitt",
+        label: "Brad Pitt",
+      },
+      {
+        value: "Catherine Deneuve",
+        label: "Catherine Deneuve",
+      },
+      {
+        value: "Charlton Heston",
+        label: "Charlton Heston",
+      },
+      {
+        value: "Choi Min-Sik",
+        label: "Choi Min-Sik",
+      },
+      {
+        value: "Christian Bale",
+        label: "Christian Bale",
+      },
+      {
+        value: "Christopher Walken",
+        label: "Christopher Walken",
+      },
+      {
+        value: "Clint Eastwood",
+        label: "Clint Eastwood",
+      },
+      {
+        value: "Daniel Day-Lewis",
+        label: "Daniel Day-Lewis",
+      },
+      {
+        value: "Denzel Washington",
+        label: "Denzel Washington",
+      },
+      {
+        value: "Dustin Hoffman",
+        label: "Dustin Hoffman",
+      },
+      {
+        value: "Edward Norton",
+        label: "Edward Norton",
+      },
+      {
+        value: "Forest Whitaker",
+        label: "Forest Whitaker",
+      },
+      {
+        value: "Gael García Bernal",
+        label: "Gael García Bernal",
+      },
+      {
+        value: "Gary Cooper",
+        label: "Gary Cooper",
+      },
+      {
+        value: "Gary Oldman",
+        label: "Gary Oldman",
+      },
+      {
+        value: "Gene Hackman",
+        label: "Gene Hackman",
+      },
+      {
+        value: "Geoffrey Rush",
+        label: "Geoffrey Rush",
+      },
+      {
+        value: "George C. Scott",
+        label: "George C. Scott",
+      },
+      {
+        value: "Gian Maria Volonté",
+        label: "Gian Maria Volonté",
+      },
+      {
+        value: "Harvey Keitel",
+        label: "Harvey Keitel",
+      },
+      {
+        value: "Henry Fonda",
+        label: "Henry Fonda",
+      },
+      {
+        value: "Humphrey Bogart",
+        label: "Humphrey Bogart",
+      },
+      {
+        value: "Isabelle Huppert",
+        label: "Isabelle Huppert",
+      },
+      {
+        value: "J.K. Simmons",
+        label: "J.K. Simmons",
+      },
+      {
+        value: "Jack Nicholson",
+        label: "Jack Nicholson",
+      },
+      {
+        value: "Jake Gyllenhaal",
+        label: "Jake Gyllenhaal",
+      },
+      {
+        value: "James Coburn",
+        label: "James Coburn",
+      },
+      {
+        value: "James Gandolfini",
+        label: "James Gandolfini",
+      },
+      {
+        value: "James Stewart",
+        label: "James Stewart",
+      },
+      {
+        value: "Joaquin Phoenix",
+        label: "Joaquin Phoenix",
+      },
+      {
+        value: "Joe Pesci",
+        label: "Joe Pesci",
+      },
+      {
+        value: "Johnny Depp",
+        label: "Johnny Depp",
+      },
+      {
+        value: "Julianne Moore",
+        label: "Julianne Moore",
+      },
+      {
+        value: "Keanu Reeves",
+        label: "Keanu Reeves",
+      },
+      {
+        value: "Kevin Costner",
+        label: "Kevin Costner",
+      },
+      {
+        value: "Kevin Spacey",
+        label: "Kevin Spacey",
+      },
+      {
+        value: "Kim Min-hee",
+        label: "Kim Min-hee",
+      },
+      {
+        value: "Leonardo DiCaprio",
+        label: "Leonardo DiCaprio",
+      },
+      {
+        value: "Lino Ventura",
+        label: "Lino Ventura",
+      },
+      {
+        value: "Mahershala Ali",
+        label: "Mahershala Ali",
+      },
+      {
+        value: "Marlon Brando",
+        label: "Marlon Brando",
+      },
+      {
+        value: "Martin Sheen",
+        label: "Martin Sheen",
+      },
+      {
+        value: "Matthew McConaughey",
+        label: "Matthew McConaughey",
+      },
+      {
+        value: "Melissa McCarthy",
+        label: "Melissa McCarthy",
+      },
+      {
+        value: "Michael B. Jordan",
+        label: "Michael B. Jordan",
+      },
+      {
+        value: "Michael Shannon",
+        label: "Michael Shannon",
+      },
+      {
+        value: "Morgan Freeman",
+        label: "Morgan Freeman",
+      },
+      {
+        value: "Nicole Kidman",
+        label: "Nicole Kidman",
+      },
+      {
+        value: "Oscar Isaac",
+        label: "Oscar Isaac",
+      },
+      {
+        value: "Philippe Noiret",
+        label: "Philippe Noiret",
+      },
+      {
+        value: "Rob Morgan",
+        label: "Rob Morgan",
+      },
+      {
+        value: "Robert De Niro",
+        label: "Robert De Niro",
+      },
+      {
+        value: "Robert Duvall",
+        label: "Robert Duvall",
+      },
+      {
+        value: "Russell Crowe",
+        label: "Russell Crowe",
+      },
+      {
+        value: "Samuel L. Jackson",
+        label: "Samuel L. Jackson",
+      },
+      {
+        value: "Saoirse Ronan",
+        label: "Saoirse Ronan",
+      },
+      {
+        value: "Sean Penn",
+        label: "Sean Penn",
+      },
+      {
+        value: "Song Kang-Ho",
+        label: "Song Kang-Ho",
+      },
+      {
+        value: "Steve Buscemi",
+        label: "Steve Buscemi",
+      },
+      {
+        value: "Sônia Braga",
+        label: "Sônia Braga",
+      },
+      {
+        value: "Tilda Swinton",
+        label: "Tilda Swinton",
+      },
+      {
+        value: "Tom Hanks",
+        label: "Tom Hanks",
+      },
+      {
+        value: "Tom Hardy",
+        label: "Tom Hardy",
+      },
+      {
+        value: "Toni Servillo",
+        label: "Toni Servillo",
+      },
+      {
+        value: "Toshirō Mifune",
+        label: "Toshirō Mifune",
+      },
+      {
+        value: "Viggo Mortensen",
+        label: "Viggo Mortensen",
+      },
+      {
+        value: "Viola Davis",
+        label: "Viola Davis",
+      },
+      {
+        value: "Wes Studi",
+        label: "Wes Studi",
+      },
+      {
+        value: "Willem Dafoe",
+        label: "Willem Dafoe",
+      },
+      {
+        value: "William Holden",
+        label: "William Holden",
+      },
+      {
+        value: "Yves Montand",
+        label: "Yves Montand",
+      },
+      {
+        value: "Zhao Tao",
+        label: "Zhao Tao",
+      },
+    ],
+  ];
